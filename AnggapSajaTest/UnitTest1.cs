2658e97a1282abf9251a4fe69f2e059129685f77
Ichwan <ichwansholihin03@gmail.com>
2024-01-29 13:51:01 +0700

    testing ci cd

--- AnggapSajaTest/UnitTest1.cs
+++ AnggapSajaTest/UnitTest1.cs
@@ -5,7 +5,9 @@ namespace AnggapSajaTest
         [Fact]
         public void Test1()
         {
-
+            int expected = 3;
+            int result = 1 + 2; 
+            result.Equals(expected);
         }
     }
 }
